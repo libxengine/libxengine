21b5ac5639a13ffb491b898f0e089d3c02eb2bfe
qyt <486179@qq.com>
2025-01-02 14:06:39 +0800

    update:new bat test

--- XEngine_WINEnv32.bat
+++ XEngine_WINEnv32.bat
@@ -1,16 +1,30 @@
 @echo off
-setlocal enabledelayedexpansion
+echo ============================
+echo operator type:
+echo   0 set XEngine_Lib32 to system environment variables(default)
+echo   1 copy module to system SysWOW64 dir
+echo ============================
+set /p user_input=input your operator type: 
 
-echo Please make sure the 64-bit environment has been entered And the 32-bit and 64-bit directories are inconsistent.
-echo Write XEngine x86-32 Library to environment variables (Y/n) [Default: Y]
+if "%user_input%"=="" (
+    set user_input=0
+)
+::query 
 
-set /p userInput=Enter Input (Y/n): 
-
-if /i "!userInput!"=="" set userInput=Y
-
-if /i "!userInput!"=="Y" (
+reg query "HKCU\Environment" /v XEngine_Include >nul 2>&1
+if %errorlevel%==0 (
+     echo environment variables XEngine_Include is existed,break.
+) else (
+    echo does not found XEngine_Include in environment variables.setting...
+    setx XEngine_Include "%cd%"
+)
+::operator
+if "%user_input%"=="1" (
+    echo select 1.copy all dll to SysWOW64...
+    for /r %%i in (*.dll) do (
+        copy "%%i" "%SystemRoot%\SysWOW64\"
+    )
+) else (
+    echo select 0
     setx XEngine_Lib32 "%cd%\XEngine_Windows"
-    echo environment variables was set.
-) 
-
-pause
+)
